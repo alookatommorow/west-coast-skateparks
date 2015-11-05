6c7fe8ed1acbb7409f6e21a7b8f40db8362cce6d
alookatommorow <hessjohnt@gmail.com>
2015-11-05 11:12:37 -0800

    reset db and modify show page to accommodate changes

--- db/migrate/20150729190222_create_skateparks.rb
+++ db/migrate/20150729190222_create_skateparks.rb
@@ -4,15 +4,20 @@
       t.string :name
       t.string :city
       t.string :state
+      t.string :identifier
       t.string :rating
+      t.string :address
+      t.string :material
       t.string :designer
       t.string :builder
       t.string :opened
-      t.string :address
       t.text :hours
       t.string :size
       t.text :notes
+      t.text :beta
       t.string :helmet
+      t.string :lights
+      t.text :obstacles
 
       t.timestamps
     end
