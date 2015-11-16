f7dbec48c4765bc883331c49d1f5171214ad2310
alookatommorow <hessjohnt@gmail.com>
2015-11-16 12:26:01 -0800

    remove outdated code in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    response = Google::Client.new.get_pics
-    # response = RestClient.get("https://www.googleapis.com/storage/v1/b/west-coast-skateparks/o?key=<%= ENV['GOOGLE_KEY'] %>")
-    p response
-  end
-
-  def google
 
   end
 end
