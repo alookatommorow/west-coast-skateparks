339abe0a626af2bd46ed5eb91d6b97d56ec8d26d
Chris Scott <christo247@gmail.com>
2016-01-09 14:35:31 -0800

    WCS spec now passing

--- spec/wcs_spec.rb
+++ spec/wcs_spec.rb
@@ -1,32 +1,21 @@
+require 'rails_helper'
+
 describe 'Homepage', type: :feature do
-
-  before do
-    Capybara.app_host = 'http://localhost:3000'
-  end
-
   it 'should load the home page' do
     visit '/'
     expect(page).to have_text('West Coast Skateparks')
   end
-
 end
 
 describe 'Show', type: :feature do
-
   it 'displays skatepark information' do
     visit '/skateparks/269'
-    expect(page).to have_text('Ojai')
-    expect(page).to have_text('Site Design')
+    expect(page).to have_text('Oceanside')
+    expect(page).to have_text('John Landes Skatepark')
   end
-
-
 end
 
 describe "the signup process", type: :feature do
-  # before :each do
-  #   User.make(username: 'fogus', email: 'user@example.com', password: 'password')
-  # end
-
   it "signs up a new user and logs them in" do
     visit '/users/new'
     fill_in 'Username', with: 'fogus'
@@ -35,6 +24,4 @@ describe "the signup process", type: :feature do
     click_button 'Register'
     expect(page).to have_content 'Page'
   end
-
-
 end
