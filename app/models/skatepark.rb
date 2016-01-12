06427978dc212958939d57ff652d025cdfff2fcc
Chris Scott <christo247@gmail.com>
2016-01-11 23:19:43 -0800

    Remove 'self.' from Skatepark model

--- app/models/skatepark.rb
+++ app/models/skatepark.rb
@@ -17,10 +17,11 @@ class Skatepark < ActiveRecord::Base
   end
 
   def pictures
-    if !self.num_pics.blank? && self.num_pics > 0
-      (1..self.num_pics).map do |i|
+    # should default num_pics to 0
+    if num_pics && num_pics > 0
+      (1..num_pics).map do |i|
         "https://storage.googleapis.com/west-coast-skateparks/"\
-        "#{self.state}/#{self.identifier}-0#{i.to_s}.jpg"
+        "#{state}/#{identifier}-0#{i.to_s}.jpg"
       end
     else
       []
@@ -30,18 +31,13 @@ class Skatepark < ActiveRecord::Base
   def visibile_attributes
     untouched = { 'Address' => self.address, 'Info' => self.info, 'Hours' => self.hours }
     titleized = {
-      'Material' => self.material, 'Designer' => self.designer,
-      'Builder' => self.builder, 'Opened' => self.opened,
-      'Size' => delimit_number,
-      'Lights' => self.lights, 'Obstacles' => self.obstacles
+      'Material' => material, 'Designer' => designer,
+      'Builder' => builder, 'Opened' => opened,
+      'Size' => delimit_size,
+      'Lights' => lights, 'Obstacles' => obstacles
     }
     titleized.each {|k, v| titleized[k] = v.titleize if v }
     untouched.merge(titleized)
-  end
-
-  # remove this method once data is properly structured
-  def delimit_number
-    "#{number_with_delimiter(self.size, delimiter: ',')} sq ft" if self.size
   end
 
   def get_lat_long
@@ -51,4 +47,10 @@ class Skatepark < ActiveRecord::Base
     lat_long.push(coords.lng)
     lat_long
   end
+
+  private
+    # remove this method once data is properly structured
+    def delimit_size
+      "#{number_with_delimiter(size, delimiter: ',')} sq ft" if size
+    end
 end
