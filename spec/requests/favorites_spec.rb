370fb7969ba843c82a08afb09e49d1053d90ec00
alookatommorow <hessjohnt@gmail.com>
2016-01-11 16:45:06 -0800

    finish add favorite spec, test passing

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -1,32 +1,15 @@
 require 'rails_helper'
 
-#add helper method for signing in
-
 describe 'user adds a favorite' do
   it 'creates a UserSkatepark and sets favorite = true' do
     user = create(:user)
     skatepark = create(:skatepark)
+
     post '/favorites', user_id: user.id, skatepark_id: skatepark.id
 
-    # UserSkatepark.where(user_id: user.id, skatepark_id: skatepark.id).first
+    user_skatepark = UserSkatepark.where(
+      user_id: user.id, skatepark_id: skatepark.id).first
+
+    expect(user_skatepark.favorite).to eq(true)
   end
-
-  # scenario 'user adds favorite' do
-  #   user = create(:user)
-  #   skatepark = create(:skatepark)
-  #   sign_in(user)
-  #   visit "/skateparks/#{skatepark.id}"
-  #   click_on('Favorite')
-  #   expect(page).to have_text('Unfavorite')
-  #   user_skatepark = UserSkatepark.where(user_id: user.id, skatepark_id: skatepark.id).first
-  #   expect(user_skatepark.favorite).to eq(true)
-  # end
-
 end
-
-# def sign_in(user)
-#   visit '/sessions/new'
-#   fill_in 'Username', with: user.username
-#   fill_in 'Password', with: user.password
-#   click_button 'Submit'
-# end
