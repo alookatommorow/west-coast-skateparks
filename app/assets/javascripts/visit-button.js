28a68d6139d2699f1700ce1e4f1c613e7125c459
alookatommorow <hessjohnt@gmail.com>
2016-01-13 21:51:43 -0800

    add ajax call for remove visit button, remove unnecessary code and files

--- app/assets/javascripts/visit-button.js
+++ app/assets/javascripts/visit-button.js
@@ -15,6 +15,21 @@ $(document).ready(function() {
     });
   });
 
+  $('.remove-visit-button').on('submit', function(event){
+    event.preventDefault();
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
+    $.ajax({ url: url, data: data, method: 'put' })
+    .done(function(response){
+      $('.remove-visit-button').addClass('hidden');
+      $('.add-visit-button').removeClass('hidden');
+      $('.opinions-container').addClass('hidden');
+    })
+    .fail(function(response){
+      console.log(response);
+    });
+  });
+
 
 
 
--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -11,8 +11,6 @@ class VisitsController < ApplicationController
       user_id: params[:user_id],
       skatepark_id: params[:skatepark_id]).first
     Visit.destroy(visit.id) if visit
-    @skatepark = Skatepark.find(params[:skatepark_id])
-
-    respond_to { |format| format.js }
+    render nothing: true
   end
 end
