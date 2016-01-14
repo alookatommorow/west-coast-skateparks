3a4aaca359b1ad2f689477fbba03f07c092ebeea
Chris Scott <christo247@gmail.com>
2016-01-13 17:21:53 -0800

    Create migration for favorites

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729190222) do
+ActiveRecord::Schema.define(version: 20160114011745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "skatepark_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "favorites", ["skatepark_id"], name: "index_favorites_on_skatepark_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "skateparks", force: true do |t|
     t.string   "name"
