3cfcacd78fbc48b71126908c2f47771e2ec3c50c
alookatommorow <hessjohnt@gmail.com>
2016-01-15 17:58:36 -0800

    create search.js and state.js and relocate corresponding js out of application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,67 +30,7 @@ function bindEvents() {
     $('.picture-modal').modal('show');
   });
 
-  //////// Show skateparks by state ////////
-  $(".park-state").on('click', function(event){
-    event.preventDefault();
-    var url = $(this).attr('href');
-    $(this).addClass('active').siblings().removeClass('active');
-    $.ajax({url: url})
-    .done(function(response) {
-      $(".parks-container").children().remove();
-      $(".parks-container").append(response)
-    })
-    .fail(function(response){
-      console.error(response)
-    })
-  });
 
-  //////// Search form submit via enter key ////////
-  $('.search-form').on('submit', function(event){
-    event.preventDefault();
-    var url = $(this).attr('action');
-    var data = $(this).serialize();
-    $.ajax({url: url, data: data})
-    .done(function(response) {
-      $(".search-results-container").remove();
-      $(".search-container").append(response);
-    })
-    .fail(function(response){
-      console.error(response);
-    })
-  });
-
-  //////// Search form submit via icon click ////////
-  $('.circular.search').on('click', function(event){
-    var url = $(this).closest('form').attr('action');
-    var data = $(this).closest('form').serialize();
-    $.ajax({url: url, data: data})
-    .done(function(response) {
-      $(".search-results-container").remove();
-      $(".search-container").append(response);
-    })
-    .fail(function(response){
-      console.error(response);
-    })
-  });
-
-  //////// Close Search ////////
-  $(".search-container").on('click', '.close-search', function(){
-    $(this).parent().slideToggle(400, function(){});
-    $('.search-form').find("input[name='search']").val('');
-  });
-
-  //////// Select Skatepark From Search ////////
-  $(".search-container").on('click', '.item', function(){
-    window.location = $(this).find("a").attr("href");
-    return false;
-  });
-
-  //////// Select Skatepark From List ////////
-  $(".parks-container").on('click', '.item', function(){
-    window.location = $(this).find("a").attr("href");
-    return false;
-  });
 
   //////// Select Skatepark From Profile Page ////////
   $(".profile-container").on('click', '.item', function(){
---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+  //////// Search form submit via enter key ////////
+  $('.search-form').on('submit', function(event){
+    event.preventDefault();
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
+    $.ajax({url: url, data: data})
+    .done(function(response) {
+      $(".search-results-container").remove();
+      $(".search-container").append(response);
+    })
+    .fail(function(response){
+      console.error(response);
+    })
+  });
+
+  //////// Search form submit via icon click ////////
+  $('.circular.search').on('click', function(event){
+    var url = $(this).closest('form').attr('action');
+    var data = $(this).closest('form').serialize();
+    $.ajax({url: url, data: data})
+    .done(function(response) {
+      $(".search-results-container").remove();
+      $(".search-container").append(response);
+    })
+    .fail(function(response){
+      console.error(response);
+    })
+  });
+
+  //////// Close Search ////////
+  $(".search-container").on('click', '.close-search', function(){
+    $(this).parent().slideToggle(400, function(){});
+    $('.search-form').find("input[name='search']").val('');
+  });
+
+  //////// Select Skatepark From Search ////////
+  $(".search-container").on('click', '.item', function(){
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+
+});
