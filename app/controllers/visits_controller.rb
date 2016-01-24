22a0f580b86a1bf2d72dfd94735ffa52c1f22875
Chris Scott <christo247@gmail.com>
2016-01-23 18:03:59 -0800

    Refactor visits controller

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,16 +1,19 @@
 class VisitsController < ApplicationController
   def create
-    Visit.create(
-      user_id: params[:user_id],
-      skatepark_id: params[:skatepark_id])
+    Visit.create(visit_params)
     render nothing: true
   end
 
   def update
-    visit = Visit.where(
-      user_id: params[:user_id],
-      skatepark_id: params[:skatepark_id]).first
+    visit = Visit.find_by(visit_params)
     Visit.destroy(visit.id) if visit
     render nothing: true
   end
+
+  private
+
+    def visit_params
+      { user_id: params[:user_id],
+      skatepark_id: params[:skatepark_id] }
+    end
 end
