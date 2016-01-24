0b8da336f9015df46397b2feb9a3e38654d935ce
Chris Scott <christo247@gmail.com>
2016-01-23 17:55:22 -0800

    Simplify OpinionsController

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,37 +1,36 @@
 class OpinionsController < ApplicationController
   def rate
-    exisiting_rating = Rating.where(
-      user_id: params[:user_id],
-      skatepark_id: params[:id]).first
-    if exisiting_rating
-      exisiting_rating.update_attributes(
-        rating: params[:rating]
-        )
+    rating = Rating.find_by(id_params)
+    if rating
+      rating.update(rating: params[:rating])
     else
-      Rating.create(
-        user_id: params[:user_id],
-        skatepark_id: params[:id],
-        rating: params[:rating])
+      Rating.create(rating_params)
     end
     redirect_to skatepark_path(params[:id])
   end
 
   def review
-    exisiting_review = Review.where(
-      user_id: params[:user_id],
-      skatepark_id: params[:id]).first
-    if exisiting_review
-      exisiting_review.update_attributes(
-        review: params[:review]
-        )
+    review = Review.find_by(id_params)
+    if review
+      review.update(review: params[:review])
     else
-      Review.create(
-        user_id: params[:user_id],
-        skatepark_id: params[:id],
-        review: params[:review])
+      Review.create(review_params)
     end
     redirect_to skatepark_path(params[:id])
   end
 
+  private
 
+  def rating_params
+    id_params.merge(rating: params[:rating])
+  end
+
+  def review_params
+    id_params.merge(review: params[:review])
+  end
+
+  def id_params
+    { user_id: params[:user_id],
+      skatepark_id: params[:id] }
+  end
 end
