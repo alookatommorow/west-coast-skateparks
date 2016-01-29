2cb066a1b19fd41d01ca57cc04cc4093e14a012a
alookatommorow <hessjohnt@gmail.com>
2016-01-28 23:03:48 -0800

    extract map for user#show page to maps.js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,3 +1,5 @@
+//skatepark#show
+
 function addNearbyParkMarkers(map, skatepark, nearbyParks) {
   if (nearbyParks.length > 0) {
     var nearbyMarkers = [];
@@ -25,6 +27,52 @@ function addNearbyParkMarkers(map, skatepark, nearbyParks) {
   });
 }
 
+
+function toggleMarkerVisibility(markers, visibility) {
+  markers.forEach(function (marker) {
+    marker.setVisible(visibility);
+  });
+}
+
+
+
+/////////  user#show
+
+var types = ['favorite', 'visited', 'both'];
+var legend = {favorite: 'purple-dot', visited: 'yellow-dot', both: 'blue-dot'}
+var toggleable = {favorite: [], visited: [], both: []};
+
+
+
+function createMarker(map, park, type) {
+  var markerPosition = {lat: park.latitude, lng: park.longitude}
+
+  var marker = new google.maps.Marker({
+    position: markerPosition,
+    map: map,
+    icon: 'https://maps.google.com/mapfiles/ms/icons/' + legend[type] + '.png',
+    title: park.city + ', ' + park.state + ' (' + type + ')'
+  });
+
+  toggleable[type].push(marker);
+}
+
+function addMarkerToggleListener(type) {
+  $('#toggle-' + type).on('click', function (event) {
+    var $button = $(event.target);
+    var action = $button.text().split(' ')
+
+    if (action[0] === 'Hide') {
+      action[0] = 'Show'
+      $button.text(action.join(' '));
+      toggleMarkerVisibility(toggleable[type], false);
+    } else {
+      action[0] = 'Hide'
+      $button.text(action.join(' '));
+      toggleMarkerVisibility(toggleable[type], true);
+    }
+  });
+}
 
 function toggleMarkerVisibility(markers, visibility) {
   markers.forEach(function (marker) {
