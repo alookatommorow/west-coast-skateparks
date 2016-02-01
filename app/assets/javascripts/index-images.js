c0a52136f6cd8fb8531079fd2362a4a5589c006e
alookatommorow <hessjohnt@gmail.com>
2016-01-31 18:38:45 -0800

    add wide div with rotating background images

--- app/assets/javascripts/index-images.js
+++ app/assets/javascripts/index-images.js
@@ -1,13 +1,24 @@
-function cycleImages(){
-    var $active = $('.image-rotator .active');
-    var $next = ($active.next().length > 0) ? $active.next() : $('.image-rotator img:first');
-    $active.fadeOut(1500,function(){
-      $active.removeClass('active');
-      $next.fadeIn(1500).addClass('active');
-  });
-}
 
 $(document).ready(function(){
-// run every 7s
-  setInterval('cycleImages()', 4000);
+  var $container = $('.image-rotator');
+  var imageUrls = ['lance-mountain', 'hewitt', 'willis', 'beckett', 'kowalski', 'drehobl', 'robbie', 'ronnie', 'flower-shop'];
+  var currentUrl = 0;
+
+  function animate(string) {
+    $container.fadeOut(1500, function() {
+      $container.css('background-image',"url('https://storage.googleapis.com/west-coast-skateparks/headers/"+string+".jpg')");
+      $container.fadeIn(1500);
+    });
+  }
+
+  function cycleImages() {
+    currentUrl++;
+    if (currentUrl == imageUrls.length - 1) {
+      currentUrl = 0;
+    }
+    animate(imageUrls[currentUrl]);
+  }
+
+  setInterval(cycleImages, 4000);
+
 });
