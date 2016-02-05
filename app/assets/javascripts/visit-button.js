9837fbadb8eb3218462c6d9f56818a7328e27162
alookatommorow <hessjohnt@gmail.com>
2016-02-04 19:17:26 -0800

    refactor visit-button.js

--- app/assets/javascripts/visit-button.js
+++ app/assets/javascripts/visit-button.js
@@ -1,37 +1,43 @@
 $(document).ready(function() {
 
-  $('.add-visit-button').on('submit', function(event){
+  function visitAjax(method, event) {
     event.preventDefault();
-    var url = $(this).attr('action');
-    var data = $(this).serialize();
-    $.ajax({ url: url, data: data, method: 'post' })
-    .done(function(response){
-      $('.remove-visit-button').removeClass('hidden');
-      $('.add-visit-button').addClass('hidden');
-      $('.opinions-container').removeClass('hidden');
+    $.ajax({
+      url: $(event.target).attr('action'),
+      data: $(event.target).serialize(),
+      method: method,
+    })
+    .done(function(response) {
+      if (method === 'post') {
+        displayRemoveVisitButton();
+      } else {
+        displayAddVisitButton();
+      }
     })
     .fail(function(response){
       console.log(response);
     });
+  }
+
+  function displayRemoveVisitButton() {
+    $('.remove-visit-button').removeClass('hidden');
+    $('.add-visit-button').addClass('hidden');
+    $('.opinions-container').removeClass('hidden');
+  }
+
+  function displayAddVisitButton() {
+    $('.remove-visit-button').addClass('hidden');
+    $('.add-visit-button').removeClass('hidden');
+    $('.opinions-container').addClass('hidden');
+  }
+
+  $('.add-visit-button').on('submit', function(event){
+    visitAjax('post', event);
   });
 
   $('.remove-visit-button').on('submit', function(event){
-    event.preventDefault();
-    var url = $(this).attr('action');
-    var data = $(this).serialize();
-    $.ajax({ url: url, data: data, method: 'put' })
-    .done(function(response){
-      $('.remove-visit-button').addClass('hidden');
-      $('.add-visit-button').removeClass('hidden');
-      $('.opinions-container').addClass('hidden');
-    })
-    .fail(function(response){
-      console.log(response);
-    });
+    visitAjax('put', event);
   });
-
-
-
 
 });
 
