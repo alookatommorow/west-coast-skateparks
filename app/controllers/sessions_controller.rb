37f852da11e6db16d2ee70998b1ef7706609d6ae
Chris Scott <christo247@gmail.com>
2016-02-07 09:31:13 -0800

    Refactor session concern

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,5 @@
 class SessionsController < ApplicationController
-
   def new
-
   end
 
   def create
@@ -9,7 +7,26 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    logout
-    redirect_to root_path
+    logout && redirect_to(root_path)
+  end
+
+  private
+
+  def login
+    if user_authenticated?
+      session[:id] = @user.id
+      redirect_to @user.is_admin? ? admin_root_path : @user
+    else
+      redirect_to new_session_path
+    end
+  end
+
+  def logout
+    session.clear
+  end
+
+  def user_authenticated?
+    @user ||= User.where(username: params[:username]).first
+    @user && @user.authenticate(params[:password])
   end
 end
