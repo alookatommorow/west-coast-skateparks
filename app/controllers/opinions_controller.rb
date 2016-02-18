4fb18a96c6413627edbb5d9b3e26fc0f440feb9f
Chris Scott <christo247@gmail.com>
2016-02-18 08:38:29 -0800

    Add association service

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,40 +1,11 @@
 class OpinionsController < ApplicationController
   def rate
-    create_opinion(:rating)
+    Association.new(Rating, params)
+    redirect_to skatepark_path(params[:id])
   end
 
   def review
-    create_opinion(:review)
+    Association.new(Review, params)
+    redirect_to skatepark_path(params[:id])
   end
-
-  private
-
-    attr_reader :type
-
-    def create_opinion(type)
-      @type = type
-      if opinion
-        opinion.update(type => params[type])
-      else
-        opinion_class.create(params_for(type))
-      end
-      redirect_to skatepark_path(params[:id])
-    end
-
-    def opinion_class
-      type == :review ? Review : Rating
-    end
-
-    def opinion
-      opinion_class.find_by(id_params)
-    end
-
-    def params_for(type)
-      id_params.merge(type => params[type])
-    end
-
-    def id_params
-      { user_id: params[:user_id],
-        skatepark_id: params[:id] }
-    end
 end
---
+++ app/services/association.rb
@@ -0,0 +1,36 @@
+class Association
+  def initialize(class_name, params)
+    @class_name = class_name
+    @params = params
+    create_association
+  end
+
+  private
+
+    attr_reader :class_name, :params
+
+    def create_association
+      if association
+        association.update(type => params[type])
+      else
+        class_name.create(params_for_type)
+      end
+    end
+
+    def type
+      class_name.name.underscore.to_sym
+    end
+
+    def association
+      class_name.find_by(id_params)
+    end
+
+    def params_for_type
+      id_params.merge(type => params[type])
+    end
+
+    def id_params
+      { user_id: params[:user_id],
+        skatepark_id: params[:id] }
+    end
+end
