805f332d518e66253608e94f4dd1d903719bce27
alookatommorow <hessjohnt@gmail.com>
2016-02-18 22:41:50 -0800

    add skatepark tests

--- spec/controllers/skateparks_controller_spec.rb
+++ spec/controllers/skateparks_controller_spec.rb
@@ -1,13 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe SkateparksController, type: :controller do
-  describe 'GET skateparks/:id/map_data' do
+  describe '#map_data' do
     it 'serves JSON object with skatepark and nearby_parks' do
       skateparks = create_list(:skatepark, 2)
       expected = skateparks.first.map_data.to_json
 
       get :map_data, id: skateparks.first.id
       expect(response.body).to eq(expected)
+    end
+  end
+
+  describe '#search' do
+    it 'renders partial with search results' do
+      get :search, search: 'lincoln'
+
+      expect(response).to render_template(partial: '_search')
+    end
+  end
+
+  describe '#state' do
+    it 'renders partial with skateparks by state' do
+      get :state, state: 'washington'
+
+      expect(response).to render_template(partial: '_state')
     end
   end
 end
