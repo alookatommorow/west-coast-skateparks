cd04162bd54cbfcce83bbf0456429cff3a513786
Chris Scott <christo247@gmail.com>
2016-02-28 20:23:08 -0800

    Move favorite and visit logci to user-actions.js

--- app/assets/javascripts/favorite-button.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-  $('.add-favorite-button').on('submit', ajaxButtonToggle);
-  $('.remove-favorite-button').on('submit', ajaxButtonToggle);
-});
--- app/assets/javascripts/user-actions.js
+++ app/assets/javascripts/user-actions.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+  $('.add-favorite-button').on('submit', ajaxButtonToggle);
+  $('.remove-favorite-button').on('submit', ajaxButtonToggle);
+
   $('.add-visit-button').on('submit', ajaxButtonToggle);
   $('.remove-visit-button').on('submit', ajaxButtonToggle);
 });
-
