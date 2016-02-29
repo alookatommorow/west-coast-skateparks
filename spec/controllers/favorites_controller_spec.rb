7c4d66695c43d998546531ed61242a7fb19e62f8
alookatommorow <hessjohnt@gmail.com>
2016-02-28 18:27:52 -0800

    convert request specs to controller specs, restore OG protect from forgery

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -7,11 +7,22 @@ RSpec.describe FavoritesController, type: :controller do
       skatepark = create(:skatepark)
       user = create(:user)
 
-      post :create, skatepark_id: skatepark.id, user_id: user_id
+      post :create, skatepark_id: skatepark.id, user_id: user.id
 
-      expect(Favorite.last.skatepark_id).to eq(skatepark_id)
-      expect(Favorite.last.user_id).to eq(user_id)
+      expect(Favorite.last.skatepark_id).to eq(skatepark.id)
+      expect(Favorite.last.user_id).to eq(user.id)
     end
   end
 
+  describe '#update' do
+    it 'destroys existing favorite' do
+      skatepark = create(:skatepark)
+      user = create(:user)
+      favorite = create(:favorite, user_id: user.id, skatepark_id: skatepark.id)
+
+      put :update, skatepark_id: skatepark.id, user_id: user.id
+
+      expect(Favorite.last).to_not be(favorite)
+    end
+  end
 end
