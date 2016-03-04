963e84c527788ce5cd951f70389ca5ded72ad908
alookatommorow <hessjohnt@gmail.com>
2016-03-04 13:56:59 -0800

    remove deprecated code from search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -16,29 +16,6 @@ $(document).ready(function(){
 
   //////// Search form submit via icon click ////////
   $('.circular.search').on('click', function(event) {
-    event.preventDefault();
-    $('#search-modal').modal('show');
-    var url = $(this).closest('form').attr('action');
-    var data = $(this).closest('form').serialize();
-    if ($('#search').val() === '') {
-      return
-    }
-    $.ajax({url: url, data: data})
-    .done(appendSearchResultsToContainer)
-    .fail(function(response){
-      console.error(response);
-    })
+    $('.search-form').submit();
   });
-
-  //////// Close Search ////////
-  $(".search-container").on('click', '.close-search', function(){
-    $(this).parent().slideToggle(400, function(){});
-    $('.search-form').find("input[name='search']").val('');
-  });
-
-  //////// Select Skatepark From Search ////////
-  // $(".search-container").on('click', '.item', function(){
-  //   makeItemClickable(this);
-  // });
-
 });
