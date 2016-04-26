8097b9e6d627c6603986acf068c9aecf74ce7682
alookatommorow <hessjohnt@gmail.com>
2016-04-26 10:09:29 -0700

    order skateparks by state

--- app/controllers/skateparks/states_controller.rb
+++ app/controllers/skateparks/states_controller.rb
@@ -2,7 +2,9 @@ module Skateparks
   class StatesController < ApplicationController
     def show
       render partial: 'skateparks/state', locals: {
-        skateparks: Skatepark.joins(:location).where("locations.state = ?", params[:state])
+        skateparks: Skatepark.joins(:location).
+        where("locations.state = ?", params[:state]).
+        order("locations.city", :name)
       }
     end
   end
