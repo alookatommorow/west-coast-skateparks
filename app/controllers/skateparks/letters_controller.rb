656d3e723b920ddf71fcaee6be501130d8680a00
alookatommorow <hessjohnt@gmail.com>
2016-05-02 19:40:07 -0700

    style letter links

--- app/controllers/skateparks/letters_controller.rb
+++ app/controllers/skateparks/letters_controller.rb
@@ -5,8 +5,8 @@ module Skateparks
         skateparks: Skatepark.
           includes(:location).
           in_state(params[:state_id]).
-          where("lower(locations.city) LIKE ?", "#{params[:letter].downcase}%").
-          order("locations.city, name"),
+          where("lower(locations.city) LIKE ?", "#{params[:letter].downcase}%")
+          order("locations.city", :name),
         state: params[:state_id],
       }
     end
