bb08897c262ad0e188d9082772ef56284e6b60c4
alookatommorow <hessjohnt@gmail.com>
2016-05-22 14:34:28 -0700

    style skateparks#show dashboard and remove gmaps autocomplete

--- app/assets/javascripts/vendor/gmaps-intializers.js
+++ app/assets/javascripts/vendor/gmaps-intializers.js
@@ -1,6 +1,5 @@
 function initAll() {
   initMap();
-  initAutocomplete();
 }
 
 function initMap() {
@@ -14,29 +13,5 @@ function initMap() {
   .fail(function(response){
     console.log("error", response);
   });
-}
-
-//// Map Autocomplete ////
-var autocomplete;
-
-function initAutocomplete() {
-  autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('autocomplete'), {types: ['geocode']});
-}
-
-function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      var circle = new google.maps.Circle({
-        center: geolocation,
-        radius: position.coords.accuracy
-      });
-      autocomplete.setBounds(circle.getBounds());
-    });
-  }
 }
 
