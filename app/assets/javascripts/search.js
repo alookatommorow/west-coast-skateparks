bacb1990ebeb0823a67abf8436f41ea9ae3742bc
alookatommorow <hessjohnt@gmail.com>
2016-05-21 22:41:37 -0700

    add style to searches

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   var appendSearchResultsToContainer = function (response) {
+    $(".ui.dimmer").removeClass("active");
     $(".search-container").empty().append(response);
   };
 
   //////// Search form submit via enter key ////////
   $('.search-form').on('submit', function(event) {
+    $(".ui.dimmer").addClass("active");
     AJAX(event, appendSearchResultsToContainer);
   });
 
