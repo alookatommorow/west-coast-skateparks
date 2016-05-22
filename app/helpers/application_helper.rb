2ffa301f81d29b6fb222fd90eb2c39761e5755b0
Chris Scott <christo247@gmail.com>
2016-05-22 10:46:25 -0700

    Do not display extra search icon on laptop view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def non_header_page?
     current_page?(root_path) ||
       current_page?(new_session_path) ||
-      current_page?(new_user_path)
+      current_page?(new_user_path) ||
+      @skatepark
   end
 end
