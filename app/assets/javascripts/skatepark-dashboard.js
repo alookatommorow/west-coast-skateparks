b2150c88d3a1acc40edfdd1a47e0352be51a974e
alookatommorow <hessjohnt@gmail.com>
2016-05-23 23:17:18 -0700

    add rudimentary photo carousel

--- app/assets/javascripts/skatepark-dashboard.js
+++ app/assets/javascripts/skatepark-dashboard.js
@@ -14,7 +14,42 @@ $("[data-dashboard-link]").click(function (event) {
   }
 });
 
-function resizeMap() {
-  var map = document.getElementById('map')
-  google.maps.event.trigger(map, 'resize');
-}
+$(document).ready(function() {
+  var currentIndex = 0,
+    item = $('.photo-carousel div').eq(currentIndex),
+    items = $('.photo-carousel div'),
+    itemAmt = items.length,
+    thumbs = $(".skatepark-show-thumbnail");
+
+  item.show();
+  thumbs.first().children().addClass("selected");
+
+  $(".prev").click(function(){
+    var item = $('.photo-carousel div').eq(currentIndex),
+        prevItem = $('.photo-carousel div').eq(currentIndex - 1)
+        prevThumb = $(".skatepark-show-thumbnail").eq(currentIndex-1).children()
+    if (currentIndex === 0) {
+      item.hide();
+      items.last().show();
+      currentIndex = itemAmt - 1;
+    } else {
+      item.hide();
+      item.prev().show();
+      currentIndex--;
+    }
+  });
+
+  $(".next").click(function(){
+    var item = $('.photo-carousel div').eq(currentIndex);
+    if (currentIndex === itemAmt - 1) {
+      item.hide();
+      items.first().show();
+      currentIndex = 0;
+    } else {
+      debugger
+      item.hide();
+      item.next().show();
+      currentIndex++;
+    }
+  });
+});
--- spec/presenters/skatepark_presenter_spec.rb
+++ spec/presenters/skatepark_presenter_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe SkateparkPresenter do
       original_skatepark = create(:skatepark, :presentable)
       skatepark = SkateparkPresenter.new(original_skatepark)
 
-      show_attributes = original_skatepark.attributes.slice('info', 'hours').merge("address" => original_skatepark.address)
+      show_attributes = original_skatepark.attributes.slice('info', 'hours')
       titleized_attributes = {
         'material' => 'Bullshit Garbage',
         'builder' => 'Bob',
