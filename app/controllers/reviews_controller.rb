5d0e4d0d602091fa5ed076a0e04ea45ee07e05d2
alookatommorow <hessjohnt@gmail.com>
2016-05-25 11:22:52 -0700

    change review form to render partial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,9 @@
 class ReviewsController < ApplicationController
   def create
     ManyToMany.create(Review, params)
-    redirect_to skatepark_path(params[:skatepark_id])
+    render partial: 'ajax-partials/reviews', locals: {
+        skatepark: Skatepark.includes({ reviews: :user }).find(params[:skatepark_id])
+      }
+    # redirect_to skatepark_path(params[:skatepark_id])
   end
 end
