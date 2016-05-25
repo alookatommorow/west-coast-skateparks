ff8fb34a972a95bd58cb0aeebb1d311549852444
alookatommorow <hessjohnt@gmail.com>
2016-05-25 10:25:18 -0700

    restyle skatepark reviews page

--- app/assets/javascripts/user-actions.js
+++ app/assets/javascripts/user-actions.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  $('button[data-slide-toggle]').each(bindSlideToggle);
   $('.ui.dropdown').dropdown(); // dropdown on rate form
 
   $("[data-ajax-container]").on("submit", "[data-ajax-form]", function (event) {
@@ -10,12 +9,5 @@ $(document).ready(function() {
 
   function renderResponse(response) {
     $(this).closest("[data-ajax-container]").html(response);
-  }
-
-  function bindSlideToggle(_, domEl) {
-    $(domEl).on('click', function (event) {
-      event.preventDefault();
-      $($(event.target).data('slide-toggle')).slideToggle(500);
-    });
   }
 });
