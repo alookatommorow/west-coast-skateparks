2097c38c795d5f5943b1c16d96b1f139b1ab19c5
alookatommorow <hessjohnt@gmail.com>
2016-06-06 22:46:18 -0700

    optimize json render in skateparks api controller

--- app/controllers/api/skateparks_controller.rb
+++ app/controllers/api/skateparks_controller.rb
@@ -1,7 +1,11 @@
 module Api
   class SkateparksController < ApplicationController
     def index
-      render json: Skatepark.includes(:location).all.to_json(include: :location)
+      render json: Skatepark.includes(:location).all.as_json(
+        only: [:id, :name],
+        include: { location:
+          { only: [:city, :state] }
+        })
     end
   end
 end
