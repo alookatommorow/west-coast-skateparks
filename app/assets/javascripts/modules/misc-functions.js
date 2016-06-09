c4dd6f03ada92d55e55fc8e4eb0dab0058652050
alookatommorow <hessjohnt@gmail.com>
2016-06-08 21:35:04 -0700

    add titleize handling for dash

--- app/assets/javascripts/modules/misc-functions.js
+++ app/assets/javascripts/modules/misc-functions.js
@@ -6,7 +6,7 @@ function displayFlashMessage() {
 }
 
 function titleize(string) {
-  var newby = capitalizeAfterSlash(string);
+  var newby = formatSpecialCharacters(string);
   return newby.split(' ').map(function(word){
     if (word.charAt(0) === '(') {
       return word.charAt(0) + word.charAt(1).toUpperCase() + word.slice(2);
@@ -16,13 +16,22 @@ function titleize(string) {
   }).join(' ');
 }
 
-function capitalizeAfterSlash(string) {
-  var indexOfSlash = string.indexOf('/');
-  if (indexOfSlash !== -1) {
-    return string.substr(0, indexOfSlash + 1) + string.charAt(indexOfSlash + 1).toUpperCase() + string.substr(indexOfSlash + 2)
+function formatSpecialCharacters(string) {
+  var slash = new RegExp('/');
+  var dash = new RegExp('-');
+  if (string.match(slash)) {
+    var slashIndex = string.match(slash).index;
+    return capitalizeAfterSpecialCharacters(string, slashIndex);
+  } else if (string.match(dash))  {
+    var dashIndex = string.match(dash).index;
+    return capitalizeAfterSpecialCharacters(string, dashIndex);
   } else {
     return string;
   }
+}
+
+function capitalizeAfterSpecialCharacters(string, index) {
+  return string.substr(0, index + 1) + string.charAt(index + 1).toUpperCase() + string.substr(index + 2)
 }
 
 var stateDisplay = {
