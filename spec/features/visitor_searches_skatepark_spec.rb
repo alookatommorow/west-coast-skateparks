fbfa648900364f2d44de04384affaa7bc03f62ec
alookatommorow <hessjohnt@gmail.com>
2016-06-14 17:54:47 -0700

    reinstate feature specs

---
+++ spec/features/visitor_searches_skatepark_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature 'Visitor searches skatepark' do
+  scenario 'they see a working link to the skatepark', js: true do
+    skatepark = create(:skatepark)
+
+    visit root_path
+    fill_in 'react-search-input', with: skatepark.name
+
+    expect(page).to have_text(skatepark.name.titleize)
+    expect(page).not_to have_text('No Results')
+
+    click_link skatepark.name.titleize
+
+    expect(page).to have_text(skatepark.address)
+  end
+end
