04675749425c3bcbf5213ccd6a630c9ec44b0216
alookatommorow <hessjohnt@gmail.com>
2016-06-23 21:59:37 -0700

    move photo js logic to new file

---
+++ app/assets/javascripts/skatepark-photos.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+  var currentIndex = 0,
+      $photos = $('.carousel-image-container div'),
+      lastIndex = $photos.length - 1,
+      $thumbs = $(".skatepark-show-thumbnail");
+
+  $photos.eq(currentIndex).show();
+  $thumbs.first().children().addClass("active-photo");
+
+  function selectNextThumb(next) {
+    $(".active-photo").removeClass("active-photo");
+    next.addClass("active-photo");
+  }
+
+  function showNext() {
+    var $currentPhoto = $photos.eq(currentIndex);
+    var $nextThumb = $thumbs.eq(currentIndex + 1).children();
+    if (currentIndex === lastIndex) {
+      $currentPhoto.hide();
+      $photos.first().show();
+      selectNextThumb($thumbs.first().children());
+      currentIndex = 0;
+    } else {
+      $currentPhoto.hide();
+      $currentPhoto.next().show();
+      selectNextThumb($nextThumb);
+      currentIndex++;
+    }
+  }
+
+  function showPrev() {
+    var $currentPhoto = $photos.eq(currentIndex),
+        $nextThumb = $thumbs.eq(currentIndex - 1).children();
+    if (currentIndex === 0) {
+      $currentPhoto.hide();
+      $photos.last().show();
+      selectNextThumb($nextThumb);
+      currentIndex = lastIndex;
+    } else {
+      $currentPhoto.hide();
+      $currentPhoto.prev().show();
+      selectNextThumb($nextThumb);
+      currentIndex--;
+    }
+  }
+
+  $(".carousel-button").click(function(){
+    if ($(this).data("carousel") === "prev") {
+      showPrev();
+    } else {
+      showNext();
+    }
+  });
+
+  $('.skatepark-show-image').click(function() {
+    showNext();
+  });
+
+  $('.skatepark-show-thumbnail').click(function() {
+    nextIndex = $(this).index();
+    var $currentPhoto = $photos.eq(currentIndex);
+    $currentPhoto.hide();
+    $photos.eq(nextIndex).show();
+    selectNextThumb($thumbs.eq(nextIndex).children());
+    currentIndex = nextIndex;
+  });
+});
