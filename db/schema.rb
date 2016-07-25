1c23ec7fe91f5c578e9ed1b7a54a66700e5be3f0
alookatommorow <hessjohnt@gmail.com>
2016-07-24 20:07:18 -0700

    store ratings as floats

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422213637) do
+ActiveRecord::Schema.define(version: 20160725020726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20160422213637) do
   create_table "ratings", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "skatepark_id"
-    t.integer  "rating"
+    t.float    "rating"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
