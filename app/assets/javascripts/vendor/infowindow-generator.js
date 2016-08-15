551b7f96f579c024ca4869868fc9c2953eba3152
alookatommorow <hessjohnt@gmail.com>
2016-08-15 15:16:36 -0700

    add logic for stars

--- app/assets/javascripts/vendor/infowindow-generator.js
+++ app/assets/javascripts/vendor/infowindow-generator.js
@@ -1,4 +1,10 @@
 function infowindowGenerator(skatepark) {
+  var stars = {};
+  var starIcons = {
+    wholeStars: "<i class='small star icon'></i>",
+    halfStars: "<i class='small star half empty icon'></i>",
+    emptyStars: "<i class='small star icon shaded'></i>"
+  }
 
   this.generateInfowindow = function() {
     return new google.maps.InfoWindow({
@@ -8,6 +14,30 @@ function infowindowGenerator(skatepark) {
 
   function generateContentString(skatepark) {
     // id='content' is a Google Maps requirement
-    return "<div id='content' style='height:50%'><div class='center-text two-bottom'><img style='height:50px' src='"+skatepark.picture+ "' ></div><strong><a href='/skateparks/"+skatepark.slug+"'>"+titleize(skatepark.name)+"</a></strong></div>";
+    countStars(skatepark.rating);
+    starIcons = generateStarHtml();
+    return "<div id='content'><a href='/skateparks/"+skatepark.slug+"'><div><img src='"+skatepark.picture+ "' ></div><strong>"+titleize(skatepark.name)+"</strong><div>" + starIcons + "</div></a></div>";
+  }
+
+  function countStars(rating) {
+    stars.wholeStars = parseInt(rating);
+    if (Number(rating) % 1 != 0) {
+      stars.halfStars = 1;
+    } else {
+      stars.halfStars = 0;
+    }
+    stars.emptyStars = 5- Math.ceil(Number(rating));
+  }
+
+  function generateStarHtml() {
+    starString = "";
+    for (var star in stars) {
+      if (stars[star] > 0) {
+        for (var i = 0; i < stars[star]; i++) {
+          starString += starIcons[star];
+        }
+      }
+    }
+    return starString;
   }
 }
