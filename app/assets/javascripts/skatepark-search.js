7390e88ef49685ae3b66ec7ec2827aea82b2b99d
alookatommorow <hessjohnt@gmail.com>
2016-08-16 09:54:30 -0700

    auto focus on search input

--- app/assets/javascripts/skatepark-search.js
+++ app/assets/javascripts/skatepark-search.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
   $(".display-search").click(function() {
     $(this).hide();
     $(".react-search-container").animate({width:'toggle'});
+    $("#react-search-input").trigger("click").focus();
   });
 });
