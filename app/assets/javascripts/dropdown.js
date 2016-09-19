78f7eebf3f432d47b374bc8472912c9f0c37718e
alookatommorow <hessjohnt@gmail.com>
2016-09-18 18:08:56 -0700

    refine dropdown styling and functionality

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,18 +1,51 @@
 $(document).ready(function() {
-  $(".dropdown, .dropdown.icon").click(function(event) {
+  var $dropdown = $(".dropdown:not(i)");
+
+  ///  toggle dropdown  ///
+  $(".dropdown:not(i), .dropdown.icon").click(function(event) {
     event.stopPropagation();
-    $(".dropdown").addClass("active").children(".menu").show();
+
+    toggleDropdown();
   });
 
-  $(document).click(function() {
-    $(".dropdown").removeClass("active").children(".menu").hide();
-  });
-
+  /// handle menu item click ///
   $(".dropdown .menu .item").click(function(event) {
     event.stopPropagation();
+
     var selectedVal = $(this).data("value");
+
     $(this).parent().siblings("input").val(selectedVal);
-    $(".dropdown").removeClass("active").children(".menu").hide();
     $(this).parent().siblings(".text").removeClass("default").text(selectedVal);
+    hideDropdownMenu();
   });
+
+  function toggleDropdown() {
+    if ($dropdown.hasClass("active")) {
+      $dropdown.children(".menu").slideToggle(200, function() {
+        $dropdown.removeClass("active upward");
+      });
+      unbindDropdownCloseListener();
+    } else {
+      var distanceToBottom = $(window).scrollTop() + $(window).height() - $dropdown.offset().top;
+      var classString = distanceToBottom < 273 ? "active upward" : "active";
+      $dropdown.addClass(classString);
+      bindDropdownCloseListener();
+      $dropdown.children(".menu").slideToggle(200);
+    }
+  }
+
+  function hideDropdownMenu() {
+    $(".dropdown:not(i)").removeClass("active upward").children(".menu").hide();
+    unbindDropdownCloseListener();
+  }
+
+  function unbindDropdownCloseListener() {
+    $(document).unbind("click");
+  }
+
+  function bindDropdownCloseListener() {
+    $(document).click(function() {
+      hideDropdownMenu();
+    });
+  }
 });
