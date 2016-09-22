306e9811cc7a08f763792ffcb5ef732631ffd7c3
alookatommorow <hessjohnt@gmail.com>
2016-09-22 14:04:15 -0700

    goodbye semantic ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require underscore
 //= require gmaps/google
-//= require semantic_ui/semantic_ui
 //= require_tree ../../../vendor/assets/javascripts/.
 //= require react
 //= require react_ujs
