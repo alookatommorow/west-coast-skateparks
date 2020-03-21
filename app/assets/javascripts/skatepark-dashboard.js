130ecaabdd48a11a73863f09407a9d38967e12d3
alookatommorow <hessjohnt@gmail.com>
2020-03-21 09:09:33 -0700

    move js back to body

--- app/assets/javascripts/skatepark-dashboard.js
+++ app/assets/javascripts/skatepark-dashboard.js
@@ -1,15 +1,17 @@
-var alreadyInitializedMap = false;
+$(document).on('turbolinks:load', function () {
+  var alreadyInitializedMap = false;
 
-$("[data-dashboard-link]").click(function (event) {
-  $(".selected").removeClass("selected");
-  $(this).addClass("selected");
-  $("[data-toggle-container]").hide();
-  $($(this).data("dashboard-link")).show();
+  $("[data-dashboard-link]").click(function (event) {
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
+    $("[data-toggle-container]").hide();
+    $($(this).data("dashboard-link")).show();
 
-  event.preventDefault();
+    event.preventDefault();
 
-  if ($(this).data("dashboard-link") === "#skatepark-map" && !alreadyInitializedMap) {
-    alreadyInitializedMap = true;
-    initMap();
-  }
+    if ($(this).data("dashboard-link") === "#skatepark-map" && !alreadyInitializedMap) {
+      alreadyInitializedMap = true;
+      initMap();
+    }
+  });
 });
