a169783fe0f38340333e459a1069eb7c2516ce6a
John Hess <hessjohnt@gmail.com>
2024-02-11 09:55:08 -0800

    remove deprecation warning from webpack config

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,19 +1,20 @@
-const { webpackConfig, merge } = require('shakapacker')
+// const { webpackConfig, merge } = require('shakapacker')
+
+const { generateWebpackConfig, merge } = require('shakapacker');
 
 const sassLoaderConfig = {
   module: {
     rules: [
       {
-        test:/.scss$/, 
-        use: ["style-loader",
-              "css-loader",
-              "sass-loader" ],
-        test: /\.css$/, 
-        use: ["style-loader",
-              "css-loader"]
-     }
+        // test: /.scss$/,
+        // use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 };
 
-module.exports = merge(webpackConfig, sassLoaderConfig);
+const webpackConfig = generateWebpackConfig(sassLoaderConfig);
+
+module.exports = merge(sassLoaderConfig, webpackConfig);
