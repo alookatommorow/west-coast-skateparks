1bf2b528df486fc50c38f88e50302ae30c57cbdf
John Hess <hessjohnt@gmail.com>
2024-02-11 21:56:14 -0800

    use protect_from_forgery null

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,4 @@ class ApplicationController < ActionController::Base
   include SessionConcern
   include Geokit::Geocoders
   protect_from_forgery with: :null_session
-  # protect_from_forgery with: :exception
 end
