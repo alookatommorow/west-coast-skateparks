6082e69dd679cd487b5b1af17f082cf9a0b4690f
John Hess <hessjohnt@gmail.com>
2024-02-20 16:03:02 -0700

    update spec

--- spec/requests/skateparks/search_spec.rb
+++ spec/requests/skateparks/search_spec.rb
@@ -3,16 +3,13 @@ require 'rails_helper'
 RSpec.describe '/skateparks' do
   describe 'GET #search' do
     it 'sets instance vars' do
-      query = 'butts'
-      ca_parks = create_list(:skatepark, 2, state: 'california').sort_by(&:city)
-      or_parks = [create(:skatepark, state: 'oregon')]
+      create_list(:skatepark, 2, state: 'california').sort_by(&:city)
+      [create(:skatepark, state: 'oregon')]
+      all_parks = Skatepark.all.order(:state, :city, :name)
 
-      get '/skateparks/search', params: { query: }
+      get '/skateparks/search'
 
-      expect(assigns(:ca_parks)).to eq json(ca_parks)
-      expect(assigns(:or_parks)).to eq json(or_parks)
-      expect(assigns(:wa_parks)).to eq []
-      expect(assigns(:query)).to eq query
+      expect(assigns(:skateparks)).to eq json(all_parks)
     end
   end
 
