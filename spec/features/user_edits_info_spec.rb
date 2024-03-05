d5a5661ac4e7f2890cd50a498b48ae2f87d3d2e9
John Hess <hessjohnt@gmail.com>
2024-03-05 12:48:48 -0800

    fix bug with edit form submission

--- spec/features/user_edits_info_spec.rb
+++ spec/features/user_edits_info_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature 'user edits info' do
-  scenario 'their info is updated' do
+  scenario 'their info is updated', js: true do
     user = create(:user, name: 'Not Steve')
 
     sign_in_user(user)
     visit user_path(user)
     click_on 'Edit Info'
-    fill_in 'Name', with: 'Steve'
-    fill_in 'Username', with: 'stevey_pants_69'
+    fill_in 'user[name]', with: 'Steve'
+    fill_in 'user[username]', with: 'stevey_pants_69'
     click_button 'Update'
 
     expect(page).to have_text 'Righteous.'
