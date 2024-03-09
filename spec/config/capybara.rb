1f4bace629201df624120867cf56634840ed7ae0
John Hess <hessjohnt@gmail.com>
2024-03-08 17:10:42 -0800

    try other snippet

--- spec/config/capybara.rb
+++ spec/config/capybara.rb
@@ -4,27 +4,34 @@ Capybara.enable_aria_label = true
 
 is_ci = ENV.fetch('CI', false)
 
+# Capybara.register_driver :ci_chrome do |app|
+#   options = Selenium::WebDriver::Chrome::Options.new(
+#     args: %w[headless no-sandbox disable-gpu disable-dev-shm-usage],
+#     binary: '/usr/bin/google-chrome'
+#   )
+
+#   Capybara::Selenium::Driver.new(
+#     app,
+#     browser: :chrome,
+#     options:
+#   )
+# end
+
 RSpec.configure do |config|
   config.after(:suite) do
     FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
   end
 
-  # tests use regular (faster) driver if they don't require js
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
-    if is_ci
-      driven_by(
-        :selenium,
-        using: :remote,
-        options: { url: 'http://localhost:4444/wd/hub', capabilities: :chrome }
-      ) do |options|
-        options.add_argument('no-sandbox')
-        options.add_argument('--headless')
-      end
-    else
+  if is_ci
+    config.before(:each, type: :system) do
+      driven_by :selenium, using: :headless_chrome
+    end
+  else
+    # tests use regular (faster) driver if they don't require js
+    config.before(:each, type: :system) do
+      driven_by :rack_test
+    end
+    config.before(:each, type: :system, js: true) do
       driven_by :selenium, using: :headless_chrome
     end
   end
