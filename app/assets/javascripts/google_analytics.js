e65f715239a4d5773746b558af4e9c3e0c9f9982
John Hess <hessjohnt@gmail.com>
2024-03-12 12:23:29 -0700

    remove arrow functions

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,5 +1,5 @@
-$(document).on('turbolinks:load', () => {
-  $('[data-ga-event]').on('click', event => {
+$(document).on('turbolinks:load', function () {
+  $('[data-ga-event]').on('click', function (event) {
     const eventParameters = $(event.currentTarget).data('ga-event');
 
     window.gtag('event', 'custom_click', eventParameters);
