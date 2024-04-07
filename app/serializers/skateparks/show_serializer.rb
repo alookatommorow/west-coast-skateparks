fad1418381d2cb3c314cd232dbdddeea3368f5e8
John Hess <hessjohnt@gmail.com>
2024-04-07 08:47:59 -0700

    add status to show serializer

--- app/serializers/skateparks/show_serializer.rb
+++ app/serializers/skateparks/show_serializer.rb
@@ -1,6 +1,6 @@
 module Skateparks
   class ShowSerializer < Skateparks::BaseSerializer
     attributes :id, :slug, :name, :city, :state, :map_photo, :stars, :obstacles, :hours, :material, :designer, :builder,
-               :opened, :size, :lights, :address, :info, :average_rating
+               :opened, :size, :lights, :address, :info, :average_rating, :status
   end
 end
