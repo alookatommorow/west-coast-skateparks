19bb48f48d759bf4906e3eef9089a586244e464c
John Hess <hessjohnt@gmail.com>
2024-05-11 09:30:11 -0700

    add lat/lng to show serializer

--- spec/serializers/skateparks/show_serializer_spec.rb
+++ spec/serializers/skateparks/show_serializer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Skateparks::ShowSerializer do
   it 'declares map attributes' do
     attributes = %i[id slug name city state map_photo stars obstacles hours material designer builder
-                    opened size lights address info average_rating status]
+                    opened size lights address info average_rating status latitude longitude]
 
     expect(Skateparks::ShowSerializer.new.attributes).to eq attributes
   end
